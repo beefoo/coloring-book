79d794e6bcc916cb4dd45b1701f7002c6edecdf8
Brian Foo <brian@youaremyjoy.org>
2016-12-12 15:00:28 -0500

    sea ice: make svg from shapfiles

--- sea_ice/sea_ice.py
+++ sea_ice/sea_ice.py
@@ -2,25 +2,152 @@
 
 import argparse
 import json
+import numpy as np
+from scipy.ndimage import gaussian_filter1d
 import shapefile # https://github.com/GeospatialPython/pyshp
+import svgwrite
+from svgwrite import inch, px
 import sys
 
 # input
 parser = argparse.ArgumentParser()
-parser.add_argument('-in', dest="INPUT_FILE", default="data/extent_N_%s_polyline_v2/extent_N_%s_polyline_v2", help="Path to input shapefiles")
+# input source: http://nsidc.org/data/docs/noaa/g02135_seaice_index/
+parser.add_argument('-input', dest="INPUT_FILE", default="data/extent_N_{month}_polygon_v2/extent_N_{month}_polygon_v2", help="Path to input shapefiles")
 parser.add_argument('-months', dest="MONTHS", default="199609,200609,201609", help="Months as a list")
+parser.add_argument('-width', dest="WIDTH", type=int, default=800, help="Width of output file")
+parser.add_argument('-output', dest="OUTPUT_FILE", default="data/extent_N_polygon_v2.svg", help="Path to output svg file")
 
 # init input
 args = parser.parse_args()
+months = args.MONTHS.split(",")
+WIDTH = args.WIDTH
 
-# convert shapefile to geojson
-sfname = args.INPUT_FILE % ("197909", "197909")
-sf = shapefile.Reader(sfname)
-fields = reader.fields[1:]
-field_names = [field[0] for field in fields]
-buf = []
-for sr in reader.shapeRecords():
-   atr = dict(zip(field_names, sr.record))
-   geom = sr.shape.__geo_interface__
-   buf.append(dict(type="Feature", geometry=geom, properties=atr))
-geojson = {"type": "FeatureCollection", "features": buf}
+# get boundaries
+def boundaries(features):
+    lngs = []
+    lats = []
+    for feature in features:
+        for coord in feature["geometry"]["coordinates"]:
+            for lnglat in coord:
+                lngs.append(lnglat[0])
+                lats.append(lnglat[1])
+    minLng = min(lngs)
+    maxLng = max(lngs)
+    minLat = min(lats)
+    maxLat = max(lats)
+    return [minLng, minLat, maxLng, maxLat]
+
+def largestPolygon(geojson):
+    coordinates = []
+    # Add features to svg
+    for i, feature in enumerate(geojson["features"]):
+        for coord in feature["geometry"]["coordinates"]:
+            coordinates.append({
+                "coordinates": coord,
+                "length": len(coord)
+            })
+    coordinates = sorted(coordinates, key=lambda c: c["length"])
+    return coordinates[-1]["coordinates"]
+
+def lnglatToPx(lnglat, bounds, width, height):
+    x = 1.0 * (lnglat[0] - bounds[0]) / (bounds[2] - bounds[0]) * width
+    y = (1.0 - 1.0 * (lnglat[1] - bounds[1]) / (bounds[3] - bounds[1])) * height
+    # return (int(round(x)), int(round(y)))
+    return (x, y)
+
+def smoothPoints(points, resolution=2, sigma=1):
+    a = np.array(points)
+
+    x, y = a.T
+    t = np.linspace(0, 1, len(x))
+    t2 = np.linspace(0, 1, len(y)*resolution)
+
+    x2 = np.interp(t2, t, x)
+    y2 = np.interp(t2, t, y)
+
+    x3 = gaussian_filter1d(x2, sigma)
+    y3 = gaussian_filter1d(y2, sigma)
+
+    x4 = np.interp(t, t2, x3)
+    y4 = np.interp(t, t2, y3)
+
+    return zip(x4, y4)
+
+def geojsonToSvg(geojson, svgfile):
+    # no features
+    features = geojson["features"]
+    if not len(features):
+        return False
+
+    # get bounds
+    bounds = boundaries(features)
+    print "Bounds %s: [%s, %s, %s, %s]" % (month, bounds[0], bounds[1], bounds[2], bounds[3])
+
+    # aspect ratio: w / h
+    aspect_ratio = 1.0 * abs(bounds[2]-bounds[0]) / abs(bounds[3]-bounds[1])
+    height = WIDTH / aspect_ratio
+
+    # Init svg
+    dwg = svgwrite.Drawing(svgfile, size=(WIDTH*px, height*px), profile='full')
+
+    # Add features to svg
+    for i, feature in enumerate(features):
+        featureId = feature["properties"]["id"]
+        for coord in feature["geometry"]["coordinates"]:
+            points = []
+            for lnglat in coord:
+                point = lnglatToPx(lnglat, bounds, WIDTH, height)
+                points.append(point)
+            points = smoothPoints(points)
+            # add closure
+            points.append((points[0][0], points[0][1]))
+            featureLine = dwg.polyline(id=featureId, points=points, stroke="#000000", stroke_width=2, fill="#ffffff")
+            dwg.add(featureLine)
+
+    # Save
+    dwg.save()
+    print "Saved svg: %s" % svgfile
+
+def shapeToGeojson(sfname):
+    # convert shapefile to geojson
+    sf = shapefile.Reader(sfname)
+    fields = sf.fields[1:]
+    field_names = [field[0] for field in fields]
+    features = []
+    for sr in sf.shapeRecords():
+        atr = dict(zip(field_names, sr.record))
+        geom = sr.shape.__geo_interface__
+        features.append({
+            "type": "Feature",
+            "geometry": geom,
+            "properties": atr
+        })
+    geojson = {"type": "FeatureCollection", "features": features}
+    return geojson
+
+features = []
+for month in months:
+    # convert shapefile to geojson
+    sfname = args.INPUT_FILE.replace("{month}", month)
+    geojson = shapeToGeojson(sfname)
+    # geojsonfilename = "data/%s.geojson" % month
+    # with open(geojsonfilename, 'w') as f:
+    #     json.dump(geojson, f, indent=2)
+
+    # reduce the set to just the largest polygon
+    polygon = largestPolygon(geojson)
+    features.append({
+        "type": "Feature",
+        "properties": { "id": "month" + month },
+        "geometry": {
+            "type": "Polygon",
+            "coordinates": [ polygon ]
+        }
+    })
+
+# create a geojson that combines all months
+combinedGeojson = {
+    "type": "FeatureCollection",
+    "features": features
+}
+geojsonToSvg(combinedGeojson, args.OUTPUT_FILE)
