eb908e917cb9274b65c3219f78bf120bcf8d8006
Brian Foo <brian@youaremyjoy.org>
2016-12-22 15:23:11 -0500

    slr: draw edges and shape

--- slr_manhattan/slr.py
+++ slr_manhattan/slr.py
@@ -32,20 +32,33 @@ DEGREES = [2, 4]
 ALPHA_THRESHOLD = 0.5
 
 def isLand(data, x, y, w, h):
+    # out of bounds
     if x < 0 or y < 0 or x >= w or y >= h:
         return False
     land = False
     (r,g,b,a) = data[y*w+x]
+    # check alpha
     if a > ALPHA_THRESHOLD:
         (h,s,v) = colorsys.rgb_to_hsv(1.0*r/255, 1.0*g/255, 1.0*b/255)
-        if 0.44 > h or h > 0.72:
+        # check for non-blue hue
+        if h < 0.44 or h > 0.72:
             land = True
     return land
 
 def isEdge(data, x, y, w, h):
     edge = False
-    neighbors = [(x-1, y-1),(x, y-1),(x+1, y-1),(x-1, y), (x+1, y), (x-1, y+1), (x, y+1), (x+1, y+1)]
+    neighbors = [
+        # (x-1, y-1),
+        (x, y-1),
+        # (x+1, y-1),
+        (x-1, y),
+        (x+1, y),
+        # (x-1, y+1),
+        (x, y+1),
+        # (x+1, y+1)
+    ]
     if isLand(data, x, y, w, h):
+        matches = 0
         for n in neighbors:
             if not isLand(data, n[0], n[1], w, h):
                 edge = True
@@ -58,6 +71,7 @@ for i,d in enumerate(DEGREES):
     im = Image.open(INPUT_FILE % d)
     data = list(im.getdata())
     (w,h) = im.size
+    edgeData = [(0,0,0) for r in range(w*h)]
 
     # get shape
     if i <= 0:
@@ -67,12 +81,14 @@ for i,d in enumerate(DEGREES):
                 (r,g,b,a) = data[y*w+x]
                 if a > ALPHA_THRESHOLD:
                     shape.append((x,y))
+                    edgeData[y*w+x] = (255,0,0)
                     break
             # go from bottom to top
             for y in range(h):
                 (r,g,b,a) = data[(h-1-y)*w+x]
                 if a > ALPHA_THRESHOLD:
-                    shape.append((x,y))
+                    shape.append((x,(h-1-y)))
+                    edgeData[(h-1-y)*w+x] = (255,0,0)
                     break
         for y in range(h):
             # go from left to right
@@ -80,21 +96,32 @@ for i,d in enumerate(DEGREES):
                 (r,g,b,a) = data[y*w+x]
                 if a > ALPHA_THRESHOLD:
                     shape.append((x,y))
+                    edgeData[y*w+x] = (255,0,0)
                     break
             # go from right to left
             for x in range(w):
                 (r,g,b,a) = data[y*w+(w-1-x)]
                 if a > ALPHA_THRESHOLD:
-                    shape.append((x,y))
+                    shape.append(((w-1-x),y))
+                    edgeData[y*w+(w-1-x)] = (255,0,0)
                     break
         # remove duplicates
         shape = list(set(shape))
         # simplify shape
-        shape = simplify(shape, 1000)
+        # shape = mu.simplify(shape, 1000)
 
     # look for white shapes
+    edges = [0] * (w*h)
     for y in range(h):
-        # go from left to right
         for x in range(w):
             if isEdge(data, x, y, w, h):
-                edge = True
+                edges[y*w+x] = 255
+
+    edgeImg = Image.new("RGB", im.size)
+    for index,px in enumerate(edgeData):
+        if edges[index] > 0:
+            edgeData[index] = (255,255,255)
+    edgeImg.putdata(edgeData)
+    edgeImg.show()
+
+    break
