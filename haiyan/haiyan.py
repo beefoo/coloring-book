9160328857d92759fd3b62c1e63ffa70a527d008
Brian Foo <brian@youaremyjoy.org>
2016-12-30 14:53:11 -0500

    haiyan: add country path

--- haiyan/haiyan.py
+++ haiyan/haiyan.py
@@ -32,33 +32,6 @@ args = parser.parse_args()
 PAD = args.PAD
 MIN_POLY_AREA = args.MIN_POLY_AREA
 
-# Retrieve country path from svg
-country_path_d = None
-country_path_w = None
-country_path_h = None
-with open(args.COUNTRY_FILE, 'rb') as f:
-    contents = f.read().replace('\n', '')
-
-    # find the path data
-    match = re.search(r'<path id="country_path" d="(.+)"/>', contents)
-    if match:
-        country_path_d = match.group(1)
-
-    # find the width and height
-    match = re.search(r'viewBox="0 0 ([0-9\.]+) ([0-9\.]+)"', contents)
-    if match:
-        country_path_w = float(match.group(1))
-        country_path_h = float(match.group(2))
-        print "Country dimensions: %s x %s" % (country_path_w, country_path_h)
-
-if country_path_d is None:
-    print "No path found in %s" % args.COUNTRY_FILE
-    sys.exit(1)
-
-if country_path_w is None or country_path_h is None:
-    print "Could not find width or height in %s" % args.COUNTRY_FILE
-    sys.exit(1)
-
 # Get image data from file
 im = Image.open(args.IMAGERY_FILE)
 data = list(im.getdata())
@@ -108,7 +81,7 @@ for i, shape in enumerate(bigShapes):
     target = 0.2 * len(points)
     points = mu.simplify(points, target)
     # smooth polygon
-    points = mu.smoothPoints(points)
+    points = mu.smoothPoints(points, resolution=2, sigma=3)
     # simplify polygon
     points = mu.simplify(points, target)
     # add closure for polygons
@@ -116,6 +89,39 @@ for i, shape in enumerate(bigShapes):
     # add polygon as polyline
     line = dwg.polyline(id="contour"+str(i), points=points, stroke="#000000", stroke_width=1, fill="none")
     dwgGroup.add(line)
+
+# Retrieve country path from svg
+country_path_d = None
+country_path_w = None
+country_path_h = None
+with open(args.COUNTRY_FILE, 'rb') as f:
+    contents = f.read().replace('\n', '')
+
+    # find the path data
+    match = re.search(r'<path id="country_path" d="(.+)"/>', contents)
+    if match:
+        country_path_d = match.group(1)
+
+    # find the width and height
+    match = re.search(r'viewBox="0 0 ([0-9\.]+) ([0-9\.]+)"', contents)
+    if match:
+        country_path_w = float(match.group(1))
+        country_path_h = float(match.group(2))
+        print "Country dimensions: %s x %s" % (country_path_w, country_path_h)
+
+if country_path_d is None:
+    print "No path found in %s" % args.COUNTRY_FILE
+    sys.exit(1)
+
+if country_path_w is None or country_path_h is None:
+    print "Could not find width or height in %s" % args.COUNTRY_FILE
+    sys.exit(1)
+
+# Add country path to svg
+t = "translate(%s,%s) scale(%s)" % (-125.0056, -117.1, 2.9024)
+countryGroup = dwg.add(dwg.g(id="country_group", transform=t))
+countryGroup.add(dwg.path(id="country", d=country_path_d, stroke="#000000", stroke_width=1.5, fill="none"))
+
 # Save
 dwg.save()
 print "Saved svg: %s" % args.OUTPUT_FILE
