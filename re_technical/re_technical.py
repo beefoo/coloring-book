4e585407f9891803f4a4fbcfdf5f259501dcd5f6
Brian Foo <brian@youaremyjoy.org>
2017-01-05 14:23:16 -0500

    re_technical: data added

---
+++ re_technical/re_technical.py
@@ -0,0 +1,12 @@
+
+
+
+# Data Sources:
+    # Total energy consumption:
+    # http://www.eia.gov/totalenergy/data/annual/
+    # Wind production:
+    # https://www.eia.gov/renewable/data.cfm#wind
+    # Solar production:
+    # https://www.eia.gov/renewable/data.cfm#summary
+    # Wind/solar potential:
+    # http://www.nrel.gov/gis/re_potential.html
