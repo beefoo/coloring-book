80b6be0dfeeb6724ca3cb4d8accb61794da0ab44
Brian Foo <brian@youaremyjoy.org>
2017-01-10 18:22:09 -0500

    add data for nuisance and equivalence

---
+++ equivalence/equivalence.py
@@ -0,0 +1,4 @@
+
+
+# Data based on "GHG Equivalencies Calculator - Calculations and References"
+# https://www.epa.gov/energy/ghg-equivalencies-calculator-calculations-and-references
---
+++ nuisance/nuisance.py
@@ -0,0 +1,9 @@
+
+# Data source for Nuisance Flood Levels
+# https://tidesandcurrents.noaa.gov/publications/NOAA_Technical_Report_NOS_COOPS_073.pdf
+
+# Data source for Sea Level Rise
+# https://tidesandcurrents.noaa.gov/sltrends/sltrends.html
+
+# Data source for water levels (for nuisance flood frequencies)
+# https://tidesandcurrents.noaa.gov/inundation/
