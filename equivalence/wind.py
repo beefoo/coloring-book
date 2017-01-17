dc0879c10c239f0da64447730e1f2cc3125ff0bc
Brian Foo <brian@youaremyjoy.org>
2017-01-17 14:39:52 -0500

    equivalence: implement wind spiral

--- equivalence/wind.py
+++ equivalence/wind.py
@@ -1,6 +1,20 @@
 # -*- coding: utf-8 -*-
 
+import math
+from shared import getDataFromSVGs
+from shared import getTransformString
 import svgwrite
+
+# Config
+PAD = 100
+CARS = ['svg/car01.svg','svg/car02.svg','svg/car03.svg','svg/car04.svg','svg/car05.svg','svg/car06.svg','svg/car07.svg','svg/car08.svg','svg/car09.svg','svg/car10.svg']
+OUTPUT_FILE = 'data/wind.svg'
+
+# Spiral config
+THETA_START = 10.0
+SPIRAL_A = 0 # turns the spiral
+SPIRAL_B = 5 # controls the distance between successive turnings.
+SPIRAL_STEP = 50
 
 # 4.73 metric tons CO2E /vehicle/year
 # https://www.epa.gov/energy/ghg-equivalencies-calculator-calculations-and-references#vehicles
@@ -15,3 +29,43 @@ vehicles = int(round(WIND_TURBINE_REDUCTIONS / VEHICLE_EMISSIONS))
 print "%s vehicles offset." % vehicles
 
 # (Should be 837)
+
+# determine the width of the svg
+maxRadius = 0
+theta = THETA_START
+x = 0
+y = 0
+a = SPIRAL_A
+b = SPIRAL_B
+for i in range(vehicles):
+    r = a + b * theta
+    x = r * math.cos(theta)
+    y = r * math.sin(theta)
+    d = math.hypot(x, y)
+    a = math.acos((2.0 * math.pow(d,2) - math.pow(SPIRAL_STEP,2)) / (2.0 * math.pow(d,2)))
+    theta += a
+    maxRadius = max([maxRadius, abs(x), abs(y)])
+
+width = int(math.ceil(maxRadius * 2))
+height = width
+print "Width: %s" % width
+cx = width * 0.5
+cy = height * 0.5
+
+# init svg
+dwg = svgwrite.Drawing(OUTPUT_FILE, size=(width+PAD*2, height+PAD*2), profile='full')
+
+theta = THETA_START
+a = SPIRAL_A
+b = SPIRAL_B
+for i in range(vehicles):
+    r = a + b * theta
+    x = cx + r * math.cos(theta)
+    y = cy + r * math.sin(theta)
+    d = math.hypot(x - cx, y - cy)
+    a = math.acos((2.0 * math.pow(d,2) - math.pow(SPIRAL_STEP,2)) / (2.0 * math.pow(d,2)))
+    theta += a
+    dwg.add(dwg.circle(center=(x+PAD, y+PAD), r=10, fill="black"))
+
+dwg.save()
+print "Saved svg: %s" % OUTPUT_FILE
