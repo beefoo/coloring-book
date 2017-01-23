d979125b77b867723994475b95013920e3b7863a
Brian Foo <brian@youaremyjoy.org>
2017-01-22 21:34:21 -0500

    ocean: add data

---
+++ ocean/ocean.py
@@ -0,0 +1,11 @@
+# Data Sources
+
+# Sea level rise:
+# https://www.star.nesdis.noaa.gov/sod/lsa/SeaLevelRise/LSA_SLR_timeseries_global.php
+
+# Ocean temperature:
+# https://www.ncdc.noaa.gov/monitoring-references/faq/anomalies.php
+
+# Ocean acidification:
+# http://hahana.soest.hawaii.edu/hot/products/products.html
+# https://www.epa.gov/climate-indicators/climate-change-indicators-ocean-acidity
