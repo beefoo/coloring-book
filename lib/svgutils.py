5bf546e3269e5fe9e114b0457244e638dbeaa359
Brian Foo <brian@youaremyjoy.org>
2017-01-24 11:35:56 -0500

    add svg utility functions

--- lib/svgutils.py
+++ lib/svgutils.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 
 import math
+import re
+
+# Utility functions
 
 def angleBetweenPoints(p1, p2):
     (x1, y1) = p1
@@ -24,8 +27,65 @@ def translatePoint(p, angle, distance):
     y2 = y + distance * math.sin(r)
     return (x2, y2)
 
+# SVG file functions
+
+def getDataFromSVG(filename):
+    paths = []
+    polygons = []
+    width = 0
+    height = 0
+    with open(filename, 'rb') as f:
+        contents = f.read().replace('\n', '')
+        # find dimensions
+        match = re.search(r'viewBox="0 0 ([0-9\.]+) ([0-9\.]+)"', contents)
+        if match:
+            width = float(match.group(1))
+            height = float(match.group(2))
+        else:
+            print "Warning: could not find dimensions for %s" % filename
+
+        # find the path data
+        matches = re.findall(r'\sd="([^"]+)"\s', contents)
+        if matches and len(matches):
+            paths = list(matches)
+        else:
+            print "Warning: couldn't find paths in %s" % filename
+
+        # find the polygon data
+        matches = re.findall(r'\spoints="([^"]+)"\s', contents)
+        if matches and len(matches):
+            polygons = list(matches)
+
+    # return data
+    return {
+        "width": width,
+        "height": height,
+        "paths": paths,
+        "polygons": polygons
+    }
+
+def getDataFromSVGs(filenames):
+    data = []
+    for filename in filenames:
+        fileData = getDataFromSVG(filename)
+        data.append(fileData)
+    return data
+
+# SVG parameter functions
+
+def getTransformString(w, h, x, y, sx=1, sy=1, r=0):
+    hw = w * 0.5
+    hh = h * 0.5
+    tx = x - hw * (sx-1)
+    ty = y - hh * (sy-1)
+    transform = "translate(%s,%s) scale(%s, %s) rotate(%s, %s, %s)" % (tx, ty, sx, sy, r, hw, hh)
+    if r==0:
+        transform = "translate(%s,%s) scale(%s, %s)" % (tx, ty, sx, sy)
+    return transform
+
 # convert points to curve
-def pointsToCurve(points, control_point_delta=0.3):
+# recommended curviness range: 0.1 - 0.5
+def pointsToCurve(points, curviness=0.3):
     commands = []
     for i, point in enumerate(points):
         # first point: move to point
@@ -35,7 +95,7 @@ def pointsToCurve(points, control_point_delta=0.3):
             # get previous and next point
             p0 = points[i-1]
             d = distance(p0, point)
-            cpd = d * control_point_delta
+            cpd = d * curviness
             p2 = None
             if i < len(points)-1:
                 p2 = points[i+1]
