3960e9c17f5df1f991a23124abd54d8b6a6b551d
Brian Foo <brian@youaremyjoy.org>
2017-02-03 11:22:53 -0500

    co2: update spacing, make into circle, add labels

--- co2/co2.py
+++ co2/co2.py
@@ -2,6 +2,7 @@
 
 import argparse
 import inspect
+import math
 import os
 import svgwrite
 import sys
@@ -15,21 +16,24 @@ import lib.mathutils as mu
 
 # input
 parser = argparse.ArgumentParser()
-# Source: http://cdiac.ornl.gov/trends/emis/tre_glob_2013.htm
+# Source: http://cdiac.ornl.gov/trends/emis/tre_glob_2013.html
+# Unit: One million metric tons of carbon
 parser.add_argument('-in', dest="INPUT_FILE", default="data/global.1751_2013.csv", help="Input file")
-parser.add_argument('-width', dest="WIDTH", type=int, default=800, help="Width")
-parser.add_argument('-height', dest="HEIGHT", type=int, default=1035, help="Height")
-parser.add_argument('-pad', dest="PAD", type=int, default=40, help="Padding")
+parser.add_argument('-width', dest="WIDTH", type=float, default=8.5, help="Width of output file")
+parser.add_argument('-height', dest="HEIGHT", type=float, default=11, help="Height of output file")
+parser.add_argument('-pad', dest="PAD", type=float, default=0.5, help="Padding of output file")
 parser.add_argument('-years', dest="YEARS", default="1800,1900,2013", help="Years")
 parser.add_argument('-rad', dest="RADIUS", type=float, default=5, help="Radius")
 parser.add_argument('-out', dest="OUTPUT_FILE", default="data/year%s.svg", help="Path pattern to output svg file")
 
 args = parser.parse_args()
-WIDTH = args.WIDTH
-HEIGHT = args.HEIGHT
-PAD = args.PAD
+DPI = 72
+PAD = args.PAD * DPI
+WIDTH = args.WIDTH * DPI - PAD * 2
+HEIGHT = args.HEIGHT * DPI - PAD * 2
 YEARS = args.YEARS.split(",")
 RADIUS = args.RADIUS
+LABEL_H = 50
 
 # Retrieve data
 data = {}
@@ -42,19 +46,27 @@ with open(args.INPUT_FILE) as f:
         cols = line.split(",")
         data[cols[0]] = int(cols[1])
 
-
-def makeSVG(filename, value):
+def makeSVG(filename, value, label):
     dwg = svgwrite.Drawing(filename, size=(WIDTH+PAD*2, HEIGHT+PAD*2), profile='full')
     dwg.defs.add(dwg.circle(id="circle", center=(0, 0), r=RADIUS, fill="#FFFFFF", stroke="#000000", stroke_width=1))
+    dwgCircles = dwg.add(dwg.g(id="circles"))
+    cx = WIDTH * 0.5 + PAD
+    cy = (HEIGHT - LABEL_H) * 0.5 + PAD + LABEL_H
+    maxR = WIDTH * 0.5 - RADIUS*2
+    # draw label
+    dwg.add(dwg.text(label, insert=(cx, PAD+LABEL_H*0.5), text_anchor="middle", alignment_baseline="before-edge", font_size=28))
+    dwg.add(dwg.circle(center=(cx, cy), r=(WIDTH * 0.5), fill="none", stroke="#000000", stroke_width=2))
+    # draw circles
     for index in range(value):
-        hx = mu.halton(index, 3)
-        hy = mu.halton(index, 5)
-        x = hx * WIDTH + PAD
-        y = hy * HEIGHT + PAD
-        dwg.add(dwg.use("#circle", insert=(x, y)))
+        ha = mu.halton(index, 3)
+        hr = mu.halton(index, 5)
+        angle = ha * 360
+        distance = hr * maxR
+        p = mu.translatePoint((cx, cy), math.radians(angle), distance)
+        dwgCircles.add(dwg.use("#circle", insert=p))
     dwg.save()
     print "Saved svg: %s" % filename
 
 for year in YEARS:
     filename = args.OUTPUT_FILE % year
-    makeSVG(filename, data[year])
+    makeSVG(filename, data[year], "%s carbon emissions from fossil fuels" % year)
