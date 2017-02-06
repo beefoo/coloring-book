1a149e7bb09be7832693a92cbda55697a7b1018c
Brian Foo <brian@youaremyjoy.org>
2017-02-06 00:14:40 -0500

    back cover: use slr data

--- back_cover/back_cover.py
+++ back_cover/back_cover.py
@@ -15,13 +15,13 @@ currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentfram
 parentdir = os.path.dirname(currentdir)
 sys.path.insert(0,parentdir)
 
-import lib.geojson as geo
-import lib.mathutils as mu
 import lib.svgutils as svgu
-
+import lib.mathutils as mu
 
 # input
 parser = argparse.ArgumentParser()
+# input source: https://www.ncdc.noaa.gov/monitoring-references/faq/anomalies.php
+parser.add_argument('-input', dest="INPUT_FILE", default="data/slr_sla_gbl_free_txj1j2_90.csv", help="Path to input file")
 parser.add_argument('-width', dest="WIDTH", type=float, default=8.5, help="Width of output file")
 parser.add_argument('-height', dest="HEIGHT", type=float, default=11, help="Height of output file")
 parser.add_argument('-pad', dest="PAD", type=float, default=0.25, help="Padding of output file")
@@ -33,16 +33,56 @@ DPI = 72
 PAD = args.PAD * DPI
 WIDTH = args.WIDTH * DPI - PAD * 2
 HEIGHT = args.HEIGHT * DPI - PAD * 2
+
+rows = []
+with open(args.INPUT_FILE, 'rb') as f:
+    lines = [line for line in f if not line.startswith("#")]
+    reader = csv.DictReader(lines, skipinitialspace=True)
+    rows = list(reader)
+
+SLR_KEY_PRIORITY = ["Jason-2", "Jason-1", "TOPEX/Poseidon", "GMSL (mm)"]
+values = []
+for row in rows:
+    for k in SLR_KEY_PRIORITY:
+        if k in row and len(row[k]):
+            value = float(row[k])
+            values.append(value)
+count = len(values)
+print "Read %s values from %s" % (count, args.INPUT_FILE)
+
+# svg config
+COMPRESS_Y = 0.6667
+COMPRESS_X = 0.99
+LINE_HEIGHT = 30.0
 COLOR = "#A92D2D"
+COLOR_ALT = "#000000"
+ADD_LINE = False
+
+# svg calculations
+chartW = WIDTH * COMPRESS_X
+chartH = HEIGHT * COMPRESS_Y
+offsetY = HEIGHT * (1-COMPRESS_Y) * 0.5
+offsetX = WIDTH * (1-COMPRESS_X) * 0.5
+
+# convert values to points
+minValue = min(values)
+maxValue = max(values)
+points = []
+for i, v in enumerate(values):
+    xp = 1.0 * i / count
+    yp = 1.0 - (v - minValue) / (maxValue - minValue)
+    x = chartW * xp + PAD + offsetX
+    y = chartH * yp + PAD + offsetY
+    points.append((x, y))
 
 # init svg
 dwg = svgwrite.Drawing(args.OUTPUT_FILE, size=(WIDTH+PAD*2, HEIGHT+PAD*2), profile='full')
 
 # water pattern
-waterW = 24
-waterH = 60
-waterStrokeW = 4
-waveHeight = 14
+waterW = 40
+waterH = 98
+waterStrokeW = 10
+waveHeight = 24
 waterPattern = dwg.pattern(id="water", patternUnits="userSpaceOnUse", size=(waterW,waterH))
 commands = svgu.patternWater(waterW, waterH, waveHeight)
 waterPattern.add(dwg.rect(size=(waterW,waterH), fill=COLOR))
@@ -56,15 +96,49 @@ dotPattern = dwg.pattern(id="dot", patternUnits="userSpaceOnUse", size=(dotSize,
 commands = svgu.patternDiamond(dotSize, dotW)
 dotPattern.add(dwg.path(d=commands, fill="#000000"))
 dwg.defs.add(dotPattern)
-dwg.add(dwg.rect(insert=(PAD, PAD), size=(WIDTH, HEIGHT), fill="url(#dot)"))
 
-data = svgu.getDataFromSVG("../sea_ice/data/extent_N_polygon_v2.svg")
-paths = [path for path in data["paths"] if len(path) > 200]
-for i, path in enumerate(paths):
-    fill = "#FFFFFF"
-    if i <= 0:
-        fill = "url(#water)"
-    dwg.add(dwg.path(d=path, fill=fill))
+# simplify points
+lineOffset = LINE_HEIGHT * 0.5
+points = mu.simplify(points, 100)
+points = mu.smoothPoints(points, 1, 2.0)
+pointsTop = [(p[0], p[1]-lineOffset) for p in points]
+pointsBottom = [(p[0], p[1]+lineOffset) for p in points]
+
+# make path commands
+x0 = PAD
+x1 = WIDTH + PAD
+y0 = HEIGHT + PAD
+y1 = PAD
+p0 = pointsTop[0]
+p1 = pointsTop[-1]
+cp = 12
+
+# top curve
+commandsTop = svgu.pointsToCurve(pointsTop, 0.1)
+commandsTop.append("Q%s,%s %s,%s" % (p1[0]+(x1-p1[0])*0.5, p1[1]-cp, x1, p1[1]))
+commandsTop.append("L%s,%s" % (x1, y1))
+commandsTop.append("L%s,%s" % (x0, y1))
+commandsTop.append("L%s,%s" % (x0, p0[1]))
+commandsTop.append("Q%s,%s %s,%s" % (x0+(p0[0]-x0)*0.5, p0[1]-cp, p0[0], p0[1]))
+dwg.add(dwg.path(d=commandsTop, fill="url(#dot)"))
+
+p0 = pointsBottom[0]
+p1 = pointsBottom[-1]
+
+# bottom curve
+commandsBottom = svgu.pointsToCurve(pointsBottom, 0.1)
+if ADD_LINE:
+    line = commandsBottom[:]
+    line.insert(0, "Q%s,%s %s,%s" % (x0+(p0[0]-x0)*0.5, p0[1]-cp, p0[0], p0[1]))
+    line.insert(0, "M%s,%s" % (x0, p0[1]))
+    line.append("Q%s,%s %s,%s" % (p1[0]+(x1-p1[0])*0.5, p1[1]-cp, x1, p1[1]))
+    dwg.add(dwg.path(d=line, fill="none", stroke=COLOR, stroke_width=20))
+commandsBottom.append("Q%s,%s %s,%s" % (p1[0]+(x1-p1[0])*0.5, p1[1]-cp, x1, p1[1]))
+commandsBottom.append("L%s,%s" % (x1, y0))
+commandsBottom.append("L%s,%s" % (x0, y0))
+commandsBottom.append("L%s,%s" % (x0, p0[1]))
+commandsBottom.append("Q%s,%s %s,%s" % (x0+(p0[0]-x0)*0.5, p0[1]-cp, p0[0], p0[1]))
+dwg.add(dwg.path(d=commandsBottom, fill="url(#water)"))
 
 dwg.save()
 print "Saved svg: %s" % args.OUTPUT_FILE
