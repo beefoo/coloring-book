43f812779d0073c4a232d79eece7a44f4fa6d208
Brian Foo <brian@youaremyjoy.org>
2017-02-06 23:07:29 -0500

    re: update patterns

--- re_technical/re_technical.py
+++ re_technical/re_technical.py
@@ -103,10 +103,17 @@ def writeSvg(filename, data):
     dwg = svgwrite.Drawing(filename, size=(WIDTH+PAD*2, HEIGHT+PAD*2), profile='full')
 
     # make patterns
-    diagonalPattern = dwg.pattern(id="diagonal", patternUnits="userSpaceOnUse", size=(20,20))
-    diagonalPattern.add(dwg.rect(insert=(0,0), size=(20,20), fill="#FFFFFF"))
-    diagonalPattern.add(dwg.path(d="M 0,20 l 20,-20 M -5,5 l 10,-10 M 15,25 l 10,-10", stroke_width=1, stroke="#000000"))
-    dwg.defs.add(diagonalPattern)
+    # diagonalPattern = dwg.pattern(id="diagonal", patternUnits="userSpaceOnUse", size=(20,20))
+    # diagonalPattern.add(dwg.rect(insert=(0,0), size=(20,20), fill="#FFFFFF"))
+    # diagonalPattern.add(dwg.path(d="M 0,20 l 20,-20 M -5,5 l 10,-10 M 15,25 l 10,-10", stroke_width=1, stroke="#000000"))
+    # dwg.defs.add(diagonalPattern)
+    scale = 0.667
+    stroke = 1.0 / 0.5
+    honeycombPattern = dwg.pattern(id="honeycomb", patternUnits="userSpaceOnUse", size=(56,100), patternTransform="scale(%s)" % scale)
+    honeycombPattern.add(dwg.rect(insert=(0,0), size=(56,100), fill="#FFFFFF"))
+    honeycombPattern.add(dwg.path(d="M28 66L0 50L0 16L28 0L56 16L56 50L28 66L28 100", fill="none", stroke="#000000", stroke_width=stroke))
+    honeycombPattern.add(dwg.path(d="M28 0L28 34L0 50L0 84L28 100L56 84L56 50L28 34", fill="none", stroke="#000000", stroke_width=stroke))
+    dwg.defs.add(honeycombPattern)
     circlesPattern = dwg.pattern(id="circles", patternUnits="userSpaceOnUse", size=(6,6))
     circlesPattern.add(dwg.rect(insert=(0,0), size=(6,6), fill="#FFFFFF"))
     circlesPattern.add(dwg.circle(center=(3,3), r=1, fill="#000000"))
@@ -115,9 +122,12 @@ def writeSvg(filename, data):
     circlesPattern.add(dwg.circle(center=(6,0), r=1, fill="#000000"))
     circlesPattern.add(dwg.circle(center=(6,6), r=1, fill="#000000"))
     dwg.defs.add(circlesPattern)
-    wavesPattern = dwg.pattern(id="waves", patternUnits="userSpaceOnUse", size=(20,20))
-    wavesPattern.add(dwg.rect(insert=(0,0), size=(20,20), fill="#FFFFFF"))
-    wavesPattern.add(dwg.path(d="M0 10 c 2.5 -5 , 7.5 -5 , 10 0 c 2.5 5 , 7.5 5 , 10 0 M -10 10 c 2.5 5 , 7.5 5 , 10 0 M 20 10 c 2.5 -5 , 7.5 -5 , 10 0", stroke_width=1, stroke="#000000", stroke_linecap="square", fill="none"))
+    # wavesPattern = dwg.pattern(id="waves", patternUnits="userSpaceOnUse", size=(20,20))
+    # wavesPattern.add(dwg.rect(insert=(0,0), size=(20,20), fill="#FFFFFF"))
+    # wavesPattern.add(dwg.path(d="M0 10 c 2.5 -5 , 7.5 -5 , 10 0 c 2.5 5 , 7.5 5 , 10 0 M -10 10 c 2.5 5 , 7.5 5 , 10 0 M 20 10 c 2.5 -5 , 7.5 -5 , 10 0", stroke_width=1, stroke="#000000", stroke_linecap="square", fill="none"))
+    wavesPattern = dwg.pattern(id="waves", patternUnits="userSpaceOnUse", size=(48,32), patternTransform="scale(0.75) rotate(45)")
+    wavesPattern.add(dwg.rect(insert=(0,0), size=(48,32), fill="#FFFFFF"))
+    wavesPattern.add(dwg.path(d="M27 32c0-3.314 2.686-6 6-6 5.523 0 10-4.477 10-10S38.523 6 33 6c-3.314 0-6-2.686-6-6h2c0 2.21 1.79 4 4 4 6.627 0 12 5.373 12 12s-5.373 12-12 12c-2.21 0-4 1.79-4 4h-2zm-6 0c0-3.314-2.686-6-6-6-5.523 0-10-4.477-10-10S9.477 6 15 6c3.314 0 6-2.686 6-6h-2c0 2.21-1.79 4-4 4C8.373 4 3 9.373 3 16s5.373 12 12 12c2.21 0 4 1.79 4 4h2z", fill="#000000"))
     dwg.defs.add(wavesPattern)
 
     shapes = dwg.add(dwg.g(id="shapes"))
@@ -138,7 +148,7 @@ actualRenewable = windProduction + solarProduction
 potentialRenewable = windPotential + solarPotential
 maxValue = max([totalEnergyConsumption, actualRenewable, potentialRenewable])
 writeSvg(args.OUTPUT_FILE, [
-    {"label": "Total energy consumption", "value": totalEnergyConsumption / maxValue, "width": 3, "pattern": "diagonal"},
+    {"label": "Total energy consumption", "value": totalEnergyConsumption / maxValue, "width": 3, "pattern": "waves"},
     {"label": "Wind and solar production", "value": actualRenewable / maxValue, "width": 3, "pattern": "circles"},
-    {"label": "Wind and solar potential", "value": potentialRenewable / maxValue, "width": 3, "pattern": "waves"}
+    {"label": "Wind and solar potential", "value": potentialRenewable / maxValue, "width": 3, "pattern": "honeycomb"}
 ])
